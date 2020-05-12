00786cc4416330fd12b03093b989ad460e0b7dc4
Phi Huynh <phihuynh1994@yahoo.com>
2020-05-12 15:55:45 -0600

    ASP.NET Core - Summaries Tutorial

---
+++ ClientApp/src/app/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent {
+  isExpanded = false;
+
+  constructor(
+    private auth: AuthService,
+  ) {}
+  
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+}
